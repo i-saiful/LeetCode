5a2ca024ffe94039073cc581baa616bad289bab7
Saiful Islam <mdsaiful.islam@bjitgroup.com>
2024-04-26 11:42:32 +0600

    [Arrays & Hashing] - 304 Problem Solve

---
+++ NeetCode/Arrays_n_Hashing/304_Range_Sum_Query_2D_Immutable/304_Range_Sum_Query_2D_Immutable.py
@@ -0,0 +1,20 @@
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        self.sum_ = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        for i, line in enumerate(matrix):
+            previous = 0
+            for j, num in enumerate(line):
+                previous += num
+                above = self.sum_[i][j + 1]
+                self.sum_[i + 1][j + 1] = previous + above
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        sum_col2 = self.sum_[row2 + 1][col2 + 1] - self.sum_[row1][col2 + 1]
+        sum_col1 = self.sum_[row2 + 1][col1] - self.sum_[row1][col1]
+        return sum_col2 - sum_col1
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
--- NeetCode/Arrays_n_Hashing/README.md
+++ NeetCode/Arrays_n_Hashing/README.md
@@ -48,3 +48,4 @@
 - [187. Repeated DNA Sequences](./187_Repeated_DNA_Sequences)
 - [380. Insert Delete GetRandom O(1)](./380_Insert_Delete_GetRandom_O1)
 - [1461. Check If a String Contains All Binary Codes of Size K](./1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K)
+- [304. Range Sum Query 2D - Immutable](./304_Range_Sum_Query_2D_Immutable)
